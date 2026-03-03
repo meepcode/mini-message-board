cf4504714260f80bf95501f8532afb0bb5ded1bc
AJ Bushman <8933768+meepcode@users.noreply.github.com>
2026-03-03 14:12:17 -0500

    Add sample messages to indexRouter

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,23 @@
 const { Router } = require("express");
 
+const messages = [
+    {
+        text: "First!",
+        user: "Alice",
+        added: new Date()
+    },
+    {
+        text: "Hi Alice!",
+        user: "Bob",
+        added: new Date()
+    },
+    {
+        text: "Hello all",
+        user: "Charlie",
+        added: new Date()
+    }
+];
+
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => res.send("Hello, World!"));
