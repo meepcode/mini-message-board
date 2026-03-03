c997923f9e18d0a2ac6f5881cf0ef5cc7dc39498
AJ Bushman <8933768+meepcode@users.noreply.github.com>
2026-03-03 14:25:05 -0500

    Display messages via EJS

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -21,7 +21,7 @@ const messages = [
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-    res.render("index", {})
+    res.render("index", { title: "Mini Messageboard", messages: messages})
 })
 
 module.exports = indexRouter;
