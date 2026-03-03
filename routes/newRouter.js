ed868d8c9ea9b365f33ef2e0091bfde27977adf4
AJ Bushman <8933768+meepcode@users.noreply.github.com>
2026-03-03 14:36:29 -0500

    Display message form on /new

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 
 const newRouter = Router();
 
-newRouter.get("/", (req, res) => res.send("New Messages"));
+newRouter.get("/", (req, res) => {
+    res.render("form", {})
+})
 
 module.exports = newRouter;
